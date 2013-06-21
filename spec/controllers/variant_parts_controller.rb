b27a1a71f9b2a2d3ad13c6ea9e5ff8d4ce98ac47
DangerDawson <david.dawson@gmail.com>
2013-06-21 15:02:20 +0100

    - refactored the tests so they are a bit more dry

--- spec/controllers/variant_parts_controller.rb
+++ spec/controllers/variant_parts_controller.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Spree::Admin::ProductPartsController do
+describe Spree::Admin::VariantPartsController do
   stub_authorization!
   let(:ability_user) { stub_model(Spree::LegacyUser, :has_spree_role? => true) }
 
@@ -21,7 +21,7 @@ describe Spree::Admin::ProductPartsController do
 
   context "#index" do
     it "list all current parts of product" do
-      spree_get :index, {:product_id => @product}
+      spree_get :index, {:variant_id => @variant.id}
       response.should be_success 
       assigns[:parts].map(&:id).should == [@variant_part.id]
     end
@@ -30,7 +30,7 @@ describe Spree::Admin::ProductPartsController do
   context "available" do
     it "list all available parts" do
       can_not_be_part = create(:product, :can_be_part => false) 
-      spree_get :available, {:q=>"%", :product_id => @product } 
+      spree_get :available, {:q=>"%", :variant_id => @variant.id } 
       response.should be_success 
       assigns[:available_products].map(&:id).should == [@product_can_be_part.id, @new_product_can_be_part.id]
     end
@@ -38,42 +38,46 @@ describe Spree::Admin::ProductPartsController do
 
   context "create" do
     it "can add a new part for a product" do
-      spree_post :create, { :format => 'js', :product_id => @product.permalink, :part_count => 2, :part_id => @new_variant_part.id } 
+      spree_post :create, { :format => 'js', :variant_id => @variant.id, :part_count => 2, :part_id => @new_variant_part.id } 
       response.should be_success 
       assigns[:part].id.should == @new_variant_part.id
 
-      spree_get :index, {:product_id => @product}
+      spree_get :index, {:variant_id => @variant.id}
       assigns[:parts].map(&:id).should == [@variant_part.id, @new_variant_part.id]
-      assigns[:parts].map { |p| @product.count_of(p) }.should == [1,2]
+      assigns[:parts].map { |p| @variant.count_of(p) }.should == [1,2]
     end
   end
 
   context "remove" do
     it "can decrement a part for a product" do
-      @product.add_part @new_variant_part, 2
+      @variant.add_part @new_variant_part, 2
 
-      spree_post :remove, { :format => 'js', :product_id => @product.permalink, :id => @new_variant_part.id } 
+      spree_post :remove, { :format => 'js', :variant_id => @variant.id, :id => @new_variant_part.id } 
       response.should be_success 
       assigns[:part].id.should == @new_variant_part.id
 
-      spree_get :index, {:product_id => @product}
+      spree_get :index, {:variant_id => @variant.id}
       assigns[:parts].map(&:id).should == [@variant_part.id, @new_variant_part.id]
-      assigns[:parts].map { |p| @product.count_of(p) }.should == [1,1]
+      assigns[:parts].map { |p| @variant.count_of(p) }.should == [1,1]
     end
   end
 
   context "set_count" do
     it "can set quantity of a part" do
-      @product.add_part @new_variant_part, 2
+      @variant.add_part @new_variant_part, 2
 
-      spree_post :set_count, { :format => 'js', :product_id => @product.permalink, :id => @new_variant_part.id, :count => 99 } 
+      spree_post :set_count, { :format => 'js', :variant_id => @variant.id, :id => @new_variant_part.id, :count => 99 } 
       response.should be_success 
       assigns[:part].id.should == @new_variant_part.id
 
-      spree_get :index, {:product_id => @product}
+      spree_get :index, {:variant_id => @variant.id}
       assigns[:parts].map(&:id).should == [@variant_part.id, @new_variant_part.id]
-      assigns[:parts].map { |p| @product.count_of(p) }.should == [1,99]
+      assigns[:parts].map { |p| @variant.count_of(p) }.should == [1,99]
     end
   end
+
+
+
+
 
 end
