5bba87be44d6a0365b89bc4c6179a967f528f910
DangerDawson <david.dawson@gmail.com>
2013-06-25 09:29:38 +0100

    - fixed labelling issue with the variants index view

--- app/overrides/add_parts_action_to_variant.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/variants/index",
-                     :name => "add_parts_action_to_variant",
-                     :replace => "[data-hook='variants_row']",
-                     :partial => "spree/admin/variants/variants_row",
-                     :disabled => false)
