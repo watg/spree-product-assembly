be25b7b3292719359ea819edbe91e8bb0a6204b5
Jerome Riga <jerome@woolandthegang.com>
2013-07-13 13:52:56 +0100

    option to zero instead of nil when kit_price_in nil

--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -1,13 +1,14 @@
 Spree::ProductsController.class_eval do
+  
   helper do
     def item_quantity(obj)
       obj.respond_to?(:count_part) ? obj.count_part : 1
     end
     def price_in_pence(obj,currency)
-      method = (obj.is_master ? :price_in : :kit_price_in) 
-      ((obj.send(method, currency).price || 0) * 100 * item_quantity(obj) ).to_i
+      method = (obj.is_master ? :price_in : :kit_price_in)
+      price = obj.send(method, currency).price || 0
+      ( price * 100 * item_quantity(obj) ).to_i
     end
-
     def currency_symbol(string)
       string[0]
     end
