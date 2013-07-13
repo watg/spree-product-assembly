f605903a2faccfbc2addbc6fea2075b79d95f9e1
Jerome Riga <jerome@woolandthegang.com>
2013-07-13 22:47:35 +0100

    remove rescue 0

--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -6,8 +6,8 @@ Spree::ProductsController.class_eval do
     end
     def price_in_pence(obj,currency)
       method = (obj.is_master ? :price_in : :kit_price_in)
-      price = obj.send(method, currency).price || 0
-      ( price * 100 * item_quantity(obj) ).to_i rescue 0
+      price = obj.send(method, currency).price 
+      ( price * 100 * item_quantity(obj) ).to_i
     end
     def currency_symbol(string)
       string[0]
