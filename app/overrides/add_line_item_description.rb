1930c86d9e28e55cb1a09ba0e4fb4f6ae4d7b8ef
Jerome Riga <jerome@woolandthegang.com>
2013-07-13 18:56:26 +0100

    st

--- app/overrides/add_line_item_description.rb
+++ app/overrides/add_line_item_description.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => "spree/orders/_line_item",
                      :name => "product_assembly_cart_item_description",
                      :replace => "[data-hook='cart_item_description']",
                      #:partial => "spree/orders/cart_description",
-                     :text => "text"
+                     :text => "text",
                      :disabled => false)
