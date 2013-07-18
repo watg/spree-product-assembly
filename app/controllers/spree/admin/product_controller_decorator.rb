32ce1047f44c97c8271b6ff118ff86f65aad779b
DangerDawson <david.dawson@gmail.com>
2013-07-18 18:02:02 +0100

    - prevent a user from deleting a variant or product that ha been used as a part

---
+++ app/controllers/spree/admin/product_controller_decorator.rb
@@ -0,0 +1,19 @@
+Spree::Admin::ProductController.class_eval do
+ 
+  def destroy
+    @product = Product.find_by_permalink!(params[:id])
+    if @product.variants_including_master.detect { |v| v.part? == true }
+      flash[:error] = Spree.t('notice_messages.product_is_part_of_an_assembly')
+      respond_with(@collection)
+    else
+      @product.destroy
+      flash[:success] = Spree.t('notice_messages.product_deleted')
+
+      respond_with(@product) do |format|
+        format.html { redirect_to collection_url }
+        format.js  { render_js_for_destroy }
+      end
+    end
+  end
+
+end
---
+++ app/controllers/spree/admin/variant_controller_decorator.rb
@@ -0,0 +1,23 @@
+Spree::Admin::VariantController.class_eval do
+
+  def destroy
+    @variant = Variant.find(params[:id])
+
+    if @variant.part? == true
+      flash[:error] = Spree.t('notice_messages.variant_is_part_of_an_assembly')
+      respond_with(@collection)
+    else
+
+      if @variant.destroy
+        flash[:success] = Spree.t('notice_messages.variant_deleted')
+      else
+        flash[:success] = Spree.t('notice_messages.variant_not_deleted')
+      end
+
+      respond_with(@variant) do |format|
+        format.html { redirect_to admin_product_variants_url(params[:product_id]) }
+        format.js  { render_js_for_destroy }
+      end
+    end
+  end
+end
